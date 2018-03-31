ed2fe5464fa00854f77e08cdb4de79c5f98da175
eggplantzzz <tmfeickert@gmail.com>
2018-03-31 09:25:52 -0700

    Add basic infrastructure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/first', (req, res, next) => {
+  res.send('The first route.');
+});
+app.get('/second', (req, res, next) => {
+  res.send('This is the second route.');
+});
+app.get('*', (req, res, next) => {
+  next({ status: 404 });
+});
+app.use((err, req, res, next) => {
+  res.send('Error handling.');
+});
