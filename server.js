b5b79dc1e8eb69cd171731aae175a7032eb4f824
eggplantzzz <tmfeickert@gmail.com>
2018-03-31 10:08:41 -0700

    test

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const branch = express();
 
-branch.get('/branch/first', (req, res, next) => {
+branch.get('/first', (req, res, next) => {
   res.send('The first route.');
 });
-branch.get('/branch/second', (req, res, next) => {
+branch.get('/second', (req, res, next) => {
   res.send('This is the second route.');
 });
-branch.get('*', (req, res, next) => {
-  const { originalUrl } = req;
-  console.log('the request url is ---> ' + originalUrl);
-  next({ status: 404 });
-});
+// branch.get('*', (req, res, next) => {
+//   const { originalUrl } = req;
+//   console.log('the request url is ---> ' + originalUrl);
+//   next({ status: 404 });
+// });
 branch.use((err, req, res, next) => {
   res.send('Error handling.');
 });
 module.exports = branch;
+// branch.listen(3000, () => console.log('other thing listening'));
