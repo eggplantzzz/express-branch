90628f73320f49d49ca33edece2d7db240e156f2
eggplantzzz <tmfeickert@gmail.com>
2018-03-31 10:12:55 -0700

    Test

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const branch = express();
 
-branch.get('/', (req, res, next) => {
-  const { originalUrl } = req;
-  console.log('the request url is ---> ' + originalUrl);
-  next({ status: 404 });
-});
 branch.get('/first', (req, res, next) => {
   res.send('The first route.');
 });
 branch.get('/second', (req, res, next) => {
   res.send('This is the second route.');
+});
+branch.get('*', (req, res, next) => {
+  const { originalUrl } = req;
+  console.log('the request url is ---> ' + originalUrl);
+  next({ status: 404 });
 });
 branch.use((err, req, res, next) => {
   res.send('Error handling.');
