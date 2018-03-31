c012a7542de6f700a63a737642cf3ce51aaa7f95
eggplantzzz <tmfeickert@gmail.com>
2018-03-31 09:40:26 -0700

    Change entry point in package.json and rename app

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const app = express();
+const branch = express();
 
-app.get('/first', (req, res, next) => {
+branch.get('/first', (req, res, next) => {
   res.send('The first route.');
 });
-app.get('/second', (req, res, next) => {
+branch.get('/second', (req, res, next) => {
   res.send('This is the second route.');
 });
-app.get('*', (req, res, next) => {
+branch.get('*', (req, res, next) => {
   next({ status: 404 });
 });
-app.use((err, req, res, next) => {
+branch.use((err, req, res, next) => {
   res.send('Error handling.');
 });
-module.exports = app;
+module.exports = branch;
